739f3926074d4f38c866ac91d22eb8dca39bf028
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-10-30 19:13:05 -0400

    Ignoring target files properly now.

--- i8080emu/target/debug/deps/i8080emu-455cf22ae9e305d2.d
+++
@@ -1,5 +0,0 @@
-/home/nick/dev/i8080emu/target/debug/deps/libi8080emu-455cf22ae9e305d2.rmeta: src/main.rs
-
-/home/nick/dev/i8080emu/target/debug/deps/i8080emu-455cf22ae9e305d2.d: src/main.rs
-
-src/main.rs:
--- i8080emu/target/debug/i8080emu.d
+++
@@ -1,1 +0,0 @@
-/home/nick/Documents/Programming/i8080emu/target/debug/i8080emu: /home/nick/Documents/Programming/i8080emu/src/main.rs
