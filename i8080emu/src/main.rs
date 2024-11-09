5e0f95ae9cb0e66930e9e809ef6920cea50a4d9e
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-11-09 17:06:49 -0500

    Added basic CLI args.

--- i8080emu/src/main.rs
+++ i8080emu/src/main.rs
@@ -1,5 +1,16 @@
+use std::env;
+
 fn main() {
-    let instructions =
-        i8080dis::disassemble_binary("/home/nick/dev/i8080emu/cpu_tests/8080PRE.COM");
-    println!("{:#?}", instructions);
+    let args: Vec<String> = env::args().collect();
+
+    let command = args[1].as_str();
+
+    match command {
+        "dis" => {
+            let instructions =
+                i8080dis::disassemble_binary(args[2].as_str());
+            println!("{:#?}", instructions);
+        },
+        _ => println!("Invalid command.")
+    }
 }
