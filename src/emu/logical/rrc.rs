b3669efa67de5e01e7e161351ca52a59fa5f4bfc
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-11-26 21:24:50 -0500

    Fixed instruction behavior.

--- src/emu/logical/rrc.rs
+++ src/emu/logical/rrc.rs
@@ -7,14 +7,12 @@ pub fn execute_rrc(state: &mut State, rrc: RRC) {
     match rrc {
         RRC::RotateRight => {
             let data = state.get_register(&Register::A);
-            let old_carry = state.alu.flags.carry as u8;
 
             // The new carry is the rightmost bit.
             let rotated_carry = data & 0b00000001 != 0;
             // Rotate right.
+            // Unlike RLC, the leftmost bit is not changed by the carry.
             let rotated = data.rotate_right(1);
-            // The old carry replaces the leftmost bit.
-            let rotated = (rotated & 0b01111111) | (old_carry << 7);
 
             // Only the carry flag is changed.
             state.set_register(&Register::A, rotated);
