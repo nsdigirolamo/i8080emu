f6445a4ddd52ac68ab0f0d6267f3af9222568918
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-11-30 17:56:03 -0500

    Minor change to arithmetic implementation in SUI.

--- src/emu/arithmetic/sui.rs
+++ src/emu/arithmetic/sui.rs
@@ -8,12 +8,11 @@ use super::get_flags;
 pub fn execute_sui(state: &mut State, sui: SUI) {
     match sui {
         SUI::SubtractImmediate { data } => {
-            // Subtraction uses two's complement, so these are signed.
-            let lhs = state.get_register(&Register::A) as i8;
-            let rhs = data as i8;
+            let lhs = state.get_register(&Register::A);
+            let (rhs, _) = (!data).overflowing_add(1); // Two's complement negation.
 
-            let (result, carried) = lhs.overflowing_sub(rhs);
-            let flags = get_flags(lhs as u8, rhs as u8, result as u8, carried);
+            let (result, carried) = lhs.overflowing_add(rhs);
+            let flags = get_flags(lhs, rhs, result, carried);
 
             state.set_register(&Register::A, result as u8);
             // Subtraction sets the carry bit if there is no carry.
