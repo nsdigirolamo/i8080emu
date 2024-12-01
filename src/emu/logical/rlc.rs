98838f99e15db27dac06a01a6855569a68c3d78e
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-11-30 21:21:20 -0500

    Fixed errors in rotation executors (for real this time)

--- src/emu/logical/rlc.rs
+++ src/emu/logical/rlc.rs
@@ -7,14 +7,11 @@ pub fn execute_rlc(state: &mut State, rlc: RLC) {
     match rlc {
         RLC::RotateLeft => {
             let data = state.get_register(&Register::A);
-            let old_carry = state.alu.flags.carry as u8;
 
             // The new carry is the leftmost bit.
             let rotated_carry = data & 0b10000000 != 0;
             // Rotate left.
             let rotated = data.rotate_left(1);
-            // The old carry replaces the rightmost bit.
-            let rotated = (rotated & 0b11111110) | old_carry;
 
             // Only the carry flag is changed.
             state.set_register(&Register::A, rotated);
--- src/emu/logical/rrc.rs
+++ src/emu/logical/rrc.rs
@@ -11,7 +11,6 @@ pub fn execute_rrc(state: &mut State, rrc: RRC) {
             // The new carry is the rightmost bit.
             let rotated_carry = data & 0b00000001 != 0;
             // Rotate right.
-            // Unlike RLC, the leftmost bit is not changed by the carry.
             let rotated = data.rotate_right(1);
 
             // Only the carry flag is changed.
