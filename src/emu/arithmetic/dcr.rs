aad8e3ba889641102c271facf504d4d574351774
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-12-03 21:25:48 -0500

    Fixed issue with dcr executor not working properly on memory.

--- src/emu/arithmetic/dcr.rs
+++ src/emu/arithmetic/dcr.rs
@@ -26,13 +26,13 @@ pub fn execute_dcr(state: &mut State, dcr: DCR) {
         DCR::DecrementMemory => {
             let address = state.get_register_pair(&RegisterPair::HL);
             // Only subtraction uses two's complement, so these are unsigned.
-            let lhs = state.get_memory(address) as i8;
-            let rhs = 1_i8;
+            let lhs = state.get_memory(address);
+            let rhs = 0b11111111; // Two's complement -1.
 
-            let (result, carried) = lhs.overflowing_sub(rhs);
-            let flags = get_flags(lhs as u8, rhs as u8, result as u8, carried);
+            let (result, carried) = lhs.overflowing_add(rhs);
+            let flags = get_flags(lhs, rhs, result, carried);
 
-            state.set_memory(address, result as u8);
+            state.set_memory(address, result);
             state.alu.flags = Flags {
                 zero: flags.zero,
                 carry: state.alu.flags.carry, // Carry flag isn't affected.
